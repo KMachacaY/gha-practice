592d2cd84ee2f83c9d3ad06a3283aa693544e6a1
KMachacaY <kenia.machaca@unosquare.com>
2026-06-26 11:00:18 -0400

    fix intentional failing test

--- src/math.test.js
+++ src/math.test.js
@@ -8,6 +8,6 @@ test('subtract: 10 - 4 equals 6', () => {
   expect(subtract(10, 4)).toBe(6);
 });
 
-test('INTENTIONAL FAIL: add returns wrong value', () => {
-  expect(add(1, 1)).toBe(999);
-}); 
+test('add: 1 + 1 equals 2', () => {
+  expect(add(1, 1)).toBe(2);
+});
